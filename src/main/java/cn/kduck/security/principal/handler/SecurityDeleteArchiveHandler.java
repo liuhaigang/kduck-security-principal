87649f929d96e6f0bbf23bf989d157490d351e6a
刘海刚 <lhg_0317@163.com>
2021-12-27 17:53:07 +0800

    微调，升级springboot版本到2.6.1

--- src/main/java/cn/kduck/security/principal/handler/SecurityDeleteArchiveHandler.java
+++ src/main/java/cn/kduck/security/principal/handler/SecurityDeleteArchiveHandler.java
@@ -3,8 +3,10 @@ package cn.kduck.security.principal.handler;
 import cn.kduck.core.dao.DefaultDeleteArchiveHandler;
 import cn.kduck.security.principal.AuthUser;
 import cn.kduck.core.service.ValueBean;
+import cn.kduck.security.principal.AuthUserHolder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.ObjectUtils;
 
 /**
  * LiuHG
@@ -13,8 +15,11 @@ public class SecurityDeleteArchiveHandler extends DefaultDeleteArchiveHandler {
 
     @Override
     protected void initValue(ValueBean valueBean) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        AuthUser user = (AuthUser)auth.getPrincipal();
-        valueBean.setValue("userName",user.getUsername());
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        AuthUser user = (AuthUser)auth.getPrincipal();
+        AuthUser authUser = AuthUserHolder.getAuthUser();
+        if(!ObjectUtils.isEmpty(authUser)) {
+            valueBean.setValue("userName", authUser.getUsername());
+        }
     }
 }
